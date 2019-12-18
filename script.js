3ee372ad52f1f420dfbbaa8febcf215e586835a9
Carlos Diaz <cdiaz86@gmail.com>
2019-12-18 11:44:38 -0500

    Prevents overwriting markedcell.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const boardContainer = document.querySelector(".board");
 
 const Gameboard = (gameboardContainer) => {
-  const board = ["test","test","test","test","test","test","test","test","test"];
+  const board = ["","","","","","","","",""];
   const boardContainer = document.querySelector(".board");
 
   const create = () => {
@@ -52,11 +52,37 @@ const Game = (gameboard, player) => {
 
   gameboard.boardContainer.addEventListener('click', (event) => {
     const tokenToPlace = players[roundNumber % 2].playerSymbol;
-    gameboard.placeToken(event.target.dataset.index, tokenToPlace);
-    roundNumber++;
+    if (gameboard.board[event.target.dataset.index] === "") {
+      gameboard.placeToken(event.target.dataset.index, tokenToPlace);
+      roundNumber++;
+    } else {
+      console.log("There is already a selection here.");  
+    }
+    console.log(`Round count is: ${roundNumber}`);
+
+
   });
 
   return {gameboard, player}
 };
 
 const newGame = Game(Gameboard(boardContainer), player);
+
+// Inside Game
+// let roundNumber = 0;
+// let players = [Player1('X'), Player2('O')];
+// const board = Board(ticTacToeContainer); // To build the board inside of 
+// ticTacToeContainer.addEventListener('click', (event) => {
+//   const tokenToPlace = players[roundNumber % 2].token;
+//   board.placeToken(event.target.value, tokenToPlace);
+//   const winner = board.checkWinner();
+//   if(winner === 1) {
+//     renderWinner({winner: player1, loser: player2});
+//   } else if (winner === -1) {
+//     renderWinner({winner: player2, loser: player1});
+//   } else if (winner === 0 && roundNumber === 9) {
+//     renderDraw(player1, player2);
+//   } else {
+//     render();
+//   }
+// });
